9c9b5e5b14fd1a2156e7c8819ab1303997e3df77
ishratnousheen <104068552+ishratnousheen@users.noreply.github.com>
2022-06-22 15:21:55 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,26 @@
+import React, { useEffect, useState } from "react";
+import "../styles/Child.css";
 
+const Selection = (props) => {
+  const { applyColor } = props;
+  const [color, setColor] = useState({ background: "" });
+
+  // useEffect(() => {
+  //   console.log("selection child triggered...");
+  //   console.log("color-child", color);
+  // });
+
+  return (
+    <>
+      <div
+        className="fix-box"
+        style={{ background: color }}
+        onClick={() => applyColor(setColor)}
+      >
+        <h2 className="subheading">Selection</h2>
+      </div>
+    </>
+  );
+};
+
+export default Selection;
